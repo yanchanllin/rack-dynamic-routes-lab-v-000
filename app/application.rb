4bbbd57ff2c968e4ebd2ca9a45f16f7e8215da09
YanC Lin <yonalin1@gmail.com>
2018-03-09 18:51:37 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,21 +1,23 @@
 class Application
 
+  @@items =[]
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path=="/items"
-      resp.write "#{Item.name}"
+    if req.path.include?"/items"
+      i=req.path.split('/').last
+      item = @@items.find {|item| item.name == i}
+      if   item == nil
+        resp.write "Item not found"
+        resp.status = 400
+      else
+        resp.write item.price
+      end
     else
       resp.write "Route not found"
       resp.status = 404
     end
-  elsif @@item.include?(item)
-       item.price
-  else
-    resp.write "Item not found"
-    resp.status = 400
-  end
   resp.finish
 end
 end
--- app/item.rb
+++ app/item.rb
@@ -1,8 +1,9 @@
 class Item
   attr_accessor :name, :price
-
+@@items = []
   def initialize(name,price)
     @name = name
     @price = price
+    @@items << self
   end
 end
