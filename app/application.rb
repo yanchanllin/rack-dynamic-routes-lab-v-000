78ea11341ff8ed3ee0f882f5e0ae6a07a89f37d7
YanC Lin <yonalin1@gmail.com>
2018-03-09 18:42:54 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -15,6 +15,7 @@ class Application
       else
         resp.write "Item not found"
         resp.status = 400
+      end
       else
         resp.write "Route not found"
         resp.status = 404
