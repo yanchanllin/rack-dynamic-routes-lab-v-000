8cf25fc5b2b79dcaeb84dfdba1642312d9d3c83a
YanC Lin <yonalin1@gmail.com>
2018-03-09 06:15:08 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,17 +1,21 @@
 class Application
 
   def call(env)
-      resp = Rack::Response.new
-      req = Rack::Request.new(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
 
-      if req.path=="/items"
-        resp.write "#{Item.name}"
-      else
-        resp.write "Route not found"
-        resp.status = 404
-      end
-    elsif @@item.include?(item)
-
-      resp.finish
+    if req.path=="/items"
+      resp.write "#{Item.name}"
+    else
+      resp.write "Route not found"
+      resp.status = 404
     end
+  elsif @@item.include?(item)
+       item.price
+  else
+    resp.write "Item not found"
+    resp.status = 400
   end
+  resp.finish
+end
+end
