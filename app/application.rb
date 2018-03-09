03105c5604890b73795962c489022b215d004bbb
YanC Lin <yonalin1@gmail.com>
2018-03-09 18:42:12 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,24 +1,26 @@
 class Application
-
- @@items =[]
+  
+  @@items =[]
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-
+    
     if req.path.include?"/items"
       i=req.path.split('/').last
       item = @@items.find {|item| item.name == i}
-        resp.write item.price
-    else
-      resp.write "Route not found"
-      resp.status = 404
+      binding.pry
+      resp.write item.price
+      if @@items.include?(req.path)
+        item.price
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      else
+        resp.write "Route not found"
+        resp.status = 404
+      end
+      
     end
-  if @@items.include?(req.path)
-       item.price
-  else
-    resp.write "Item not found"
-    resp.status = 400
+    resp.finish
   end
-  resp.finish
-end
 end
